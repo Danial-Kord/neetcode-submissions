3af0e96d481fc019a75e49d4a26551d7f5dd00b7
Danial Kordmodanlou <danial.kordmodanlou@gmail.com>
2026-05-21 19:57:56 -0400

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int FindDuplicate(int[] nums) {
+        HashSet<int> set = new HashSet<int>();
+
+        for(int i = 0; i < nums.Length; i++){
+            if(set.Contains(nums[i]))
+                return nums[i];
+            set.Add(nums[i]);
+        }
+        return 0;   
+    }
+}
