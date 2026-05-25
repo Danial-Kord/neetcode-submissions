3aaf54525b88d0901d7c33aa454c5f7e31660cd3
Danial Kordmodanlou <danial.kordmodanlou@gmail.com>
2026-05-24 22:40:03 -0400

    Add: kth-largest-integer-in-a-stream - submission-6

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-6.cs
@@ -0,0 +1,19 @@
+public class KthLargest {
+
+    PriorityQueue<int,int> minHeap;
+    int k;
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        minHeap = new ();
+        foreach (int i in nums)
+            minHeap.Enqueue(i, i);
+    }
+    
+    public int Add(int val) {
+        minHeap.Enqueue(val, val);
+        while(minHeap.Count > k){
+            minHeap.Dequeue();
+        }
+        return minHeap.Peek();
+    }
+}
